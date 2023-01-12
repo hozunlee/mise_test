bcab8854d62be319e0757738ca7786da095055e2
hozunlee <ho2yahh@gmail.com>
2023-01-12 19:05:38 +0900

    feat : recoil connect

---
+++ src/utils/store.js
@@ -0,0 +1,13 @@
+import { atom } from "recoil";
+
+const doingState = atom({
+    key: "doingState",
+    default: {},
+});
+
+const doneState = atom({
+    key: "doneState",
+    default: {},
+});
+
+export { doingState, doneState };
