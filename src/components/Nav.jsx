2c47543f17ef349eb878c31454347058adcfa1b3
hozunlee <ho2yahh@gmail.com>
2023-01-12 14:31:16 +0900

    feat : nav 컴포넌트 구성

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+const Nav = () => {
+    return (
+        <nav className="">
+            <div className="mx-auto max-w-6xl px-4 shadow-xl ">
+                <div className="flex justify-between items-center text-black ">
+                    <button className="my-2 space-x-1 p-2 font-bold">
+                        <i className="ri-arrow-left-line"></i>
+                    </button>
+                    <p>
+                        <b>기말고사 총 범위</b>
+                    </p>
+                    <button className="my-2 space-x-1 p-2 font-bold">
+                        <i className="ri-more-line"></i>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
