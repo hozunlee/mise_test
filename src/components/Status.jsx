4fe3d0067dd771bdb5f76b1458ca82d2d0e1c979
hozunlee <ho2yahh@gmail.com>
2023-01-12 19:06:11 +0900

    style : nav style

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,10 +1,10 @@
 const Status = () => {
     return (
-        <div className="flex m-2 justify-around h-16 items-center">
-            <div className="flex border p-3 w-32 justify-between rounded-lg bg-gray-100  text-sm h-12 ">
+        <div className="flex justify-between h-16 items-center">
+            <div className="flex border p-3 mr-1 w-full justify-between rounded-lg bg-gray-100  text-sm h-12 ">
                 <p>0/70단어</p> <p className="text-gray-400">0%</p>
             </div>
-            <button className="border w-32  rounded-lg bg-blue-50 text-sm text-blue-400 font-bold h-12 ">
+            <button className="border w-full ml-1 rounded-lg bg-blue-50 text-sm text-blue-400 font-bold h-12 ">
                 큰 카드로 보기
             </button>
         </div>
