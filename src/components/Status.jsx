8f4886a205ebbca2baf81b5eefca222942bcbf6f
hozunlee <ho2yahh@gmail.com>
2023-01-13 16:08:23 +0900

    fix : bug totalCount fix

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -4,14 +4,16 @@ import { doingState, doneState } from "../utils/store";
 const Status = () => {
     const doingCount = useRecoilValue(doingState).length;
     const doneCount = useRecoilValue(doneState).length ?? 0;
-    const doneLate = (doneCount / doingCount) * 100;
+
+    const totalCount = doneCount + doingCount;
+    const doneLate = (doneCount / totalCount) * 100;
     return (
         <div className="flex justify-between h-16 items-center">
             <div className="flex border p-3 mr-1 w-full justify-between rounded-lg bg-gray-100  text-sm h-12 ">
                 <p>
-                    {doneCount}/{doingCount}단어
+                    {doneCount}/{totalCount}단어
                 </p>{" "}
-                <p className="text-gray-400">{doneLate}%</p>
+                <p className="text-gray-400">{doneLate.toFixed()}%</p>
             </div>
             <button className="border w-full ml-1 rounded-lg bg-blue-50 text-sm text-blue-400 font-bold h-12 ">
                 큰 카드로 보기
