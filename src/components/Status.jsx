bb2ede6d48a7f5d6c9e1bb7d77eaeca0d120a522
hozunlee <ho2yahh@gmail.com>
2023-01-13 15:31:39 +0900

    feat : status bar data 입력

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,8 +1,17 @@
+import { useRecoilValue } from "recoil";
+import { doingState, doneState } from "../utils/store";
+
 const Status = () => {
+    const doingCount = useRecoilValue(doingState).length;
+    const doneCount = useRecoilValue(doneState).length ?? 0;
+    const doneLate = (doneCount / doingCount) * 100;
     return (
         <div className="flex justify-between h-16 items-center">
             <div className="flex border p-3 mr-1 w-full justify-between rounded-lg bg-gray-100  text-sm h-12 ">
-                <p>0/70단어</p> <p className="text-gray-400">0%</p>
+                <p>
+                    {doneCount}/{doingCount}단어
+                </p>{" "}
+                <p className="text-gray-400">{doneLate}%</p>
             </div>
             <button className="border w-full ml-1 rounded-lg bg-blue-50 text-sm text-blue-400 font-bold h-12 ">
                 큰 카드로 보기
